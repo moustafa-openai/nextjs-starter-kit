60eba1c6b80b932038af36896a33ad59222de130
Moustafa Elhadary <moustafa@openai.com>
2025-09-23 23:58:08 -0700

    prune all

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // allowedDevOrigins: ["https://sb-*.vercel.run", "*"],
+  /* config options here */
 };
 
 export default nextConfig;
